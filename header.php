9909fd4304d61b00edeac0d1d0560315bca6d991
Alexandre Menin <alex.menin11@gmail.com>
2020-08-12 14:54:34 -0300

    Update header.php

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no">
 	<meta name="description" content="<?php bloginfo('description') ?>">
 	<meta name="url" content="<?php bloginfo('url') ?>">
 
